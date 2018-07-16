42dd24442667965399efb2905149e41feb742990
mustardjelly <32181718+mustardjelly@users.noreply.github.com>
2018-07-16 12:33:48 +1200

    Change Tasky ranges, add next task function

--- TaskList.py
+++ TaskList.py
@@ -1,32 +1,39 @@
 import json
 
 class TaskList:
-	def __init__(self):
+	def __init__(self, value = 3):
+		self.size = value
 		self.count = 0
-		self.taskList = ['' for i in range(3)]
+		self.taskList = ['' for i in range(value)]
 		
 	def __str__(self):
 		return 'Task List: {0}\nCount: {1}'.format(self.taskList, self.count) 
 		
 	def populate_list(self):
-		for i in range(3):
+		for i in range(self.size):
 			self.taskList[i] = str(input("Enter task {0}:".format(i + 1)))
 			while (self.taskList[i] == ""):
 				print("No input detected!")
 				self.taskList[i] = str(input("Enter task {0}:".format(i + 1)))
 				
 	def inc_count(self):
-		self.count = (self.count + 1) % 3
+		self.count = (self.count + 1) % self.size
 		
 	def set_count(self, value):
-		self.count = int(value) % 3
+		self.count = int(value) % self.size
 		
 	def get_count(self):
 		return self.count
 	
+	def set_size(self, value = 3):
+		self.size = value
+		
+	def get_size(self):
+		return self.size
+	
 	def add_task(self, task, index = 'default'):
 		if (index == 'default'):
-			index = self.count % 3
+			index = self.count % self.size
 		self.taskList[index] = task
 		self.inc_count()
 		
@@ -40,8 +47,13 @@ class TaskList:
 		self.inc_count()
 		
 	def get_current_task(self):
-		curr_index = self.count % 3
+		curr_index = self.count % self.size
 		return self.taskList[curr_index]
+		
+	def get_next_task(self):
+		curr_index = self.count % self.size
+		return self.taskList[curr_index + 1]
+		
 		
 	def get_tasks(self):
 		return self.taskList
--- Tasky.py
+++ Tasky.py
@@ -7,23 +7,20 @@ SAVE_LOC = 'tasky_save.txt'
 
 #Tasky
 def load():
-	print('Welcome to Tasky')
-	task_list = TaskList()
-	try:
-		file_obj = open(SAVE_LOC, 'rb')
-		task_list = pickle.load(file_obj)
-		print('Previous tasks loaded!')
-	except():
-		print('here')
-		task_list.populate_list()
+	file_obj = open(SAVE_LOC, 'rb')
+	task_list = pickle.load(file_obj)
+	print('Previous tasks loaded!')
 	return task_list
 
 def main_screen(task_list):
 	current_task = task_list.get_current_task()
-	print('The current time is:')
+	next_index = task_list.get_count() + 1
+	next_task = task_list.get_next_task()
+	
+	print('The current time is: ', end='')
 	print('{0}'.format(time.strftime('%X')))
-	print('Current Task: \n{0}'.format(current_task))
-	print('Task List: {0}'.format(task_list.get_tasks()))
+	print('Current Task: {0}'.format(current_task))
+	print('Next Task: {0}'.format(next_task))
 	
 def add_task(task_list):
 	task = str(input('Next Task (x to e(X)it):\n'))
@@ -47,10 +44,10 @@ def add_task(task_list):
 		task_list.add_task(task)
 		return True
 
-def confirm()
+def confirm():
 	confirm = str(input('Are you sure (y/n)?\n')).lower()
-	while (confirm != 'y' or confirm != 'n'):
-		confirm = str(input('Invalid Answer\nAre you sure? (y/n)?\n')).lower()
+	while (confirm != 'y' and confirm != 'n'):
+		confirm = str(input('Invalid Answer\nAre you sure? (y/n)?')).lower()
 	if (confirm == 'y'):
 		return True
 	else:
@@ -60,9 +57,29 @@ def save(task_list):
 	with open(SAVE_LOC, 'wb') as handle:
 		pickle.dump(task_list, handle, -1)
 	print('Tasky Saved!')
+	
+def new_or_load():
+	type_input = str(input('(N)ew List or (L)oad List?\n'))
+	while (type_input[0] != 'n' and type_input[0] != 'l'):
+		type_input = str(input('(N)ew List or (L)oad List?')).lower()
+	if (type_input[0] == 'n'):
+		can_continue = False
+		while (not(can_continue)):
+			try:
+				int_input = int(input('Number of tasks in the new list? (default is 3)'))
+				if (int_input > 0):
+					can_continue = True
+			except:
+				print("Must be an integer!")
+		task_list = TaskList(int_input)
+		task_list.populate_list()
+		return task_list
+	else:
+		return load()
 
 def main():
-	task_list = load()
+	print('Welcome to Tasky')
+	task_list = new_or_load()
 	main_screen(task_list)
 	
 	while (add_task(task_list)):
